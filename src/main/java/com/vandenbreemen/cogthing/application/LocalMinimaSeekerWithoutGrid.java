351d5fdd5ead57918478270609c4a1574516b1e4
kevin <kevin.vandenbreemen@gmail.com>
2021-01-26 20:23:01 -0500

    Begin work on light-weight local minima seeker that doesn't rely on a grid object

---
+++ src/main/java/com/vandenbreemen/cogthing/application/LocalMinimaSeekerWithoutGrid.java
@@ -0,0 +1,141 @@
+package com.vandenbreemen.cogthing.application;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Minima seeker that does not employ a grid (grids won't work beyond a couple of dimensions due to memory constraints)
+ */
+public class LocalMinimaSeekerWithoutGrid {
+
+    private static final byte FORWARD = 1;
+    private static final byte BACKWARD = 2;
+    private static final byte BACKWARD_FORWARD = 3;
+
+    private int numDimensions;
+    private int numPoints;
+    private int[] currentLocationInSpace;
+
+    /**
+     * Cost / calculated values surrounding the {@link #currentLocationInSpace}
+     */
+    private double[] adjacentValues;
+
+    public LocalMinimaSeekerWithoutGrid(int numDimensions, int numPoints) {
+        this.numDimensions = numDimensions;
+        this.numPoints = numPoints;
+    }
+
+    public void setCurrentLocationInSpace(int ... currentLocationInSpace) {
+        this.currentLocationInSpace = currentLocationInSpace;
+    }
+
+    public void setAdjacentValues(double ... adjacentValues) {
+        this.adjacentValues = adjacentValues;
+    }
+
+    double sigmoid(double x) {
+        return (1/( 1 + Math.pow(Math.E,( (-8*x) +4))));
+    }
+
+    public int[] getNextLocation() {
+
+        //  First calculate the move costs along each direction
+        double[] moveCosts = new double[numDimensions*2];
+        double sum;
+        for(int dimension = 0; dimension<numDimensions; dimension++) {
+
+            double moveBackRaw = adjacentValues[2*dimension];
+            double moveForwRaw = adjacentValues[(2*dimension) + 1];
+
+            sum = 0;
+            sum += moveBackRaw;
+            moveCosts[2*dimension] = sigmoid(sum);
+
+            sum = 0;
+            sum += moveForwRaw;
+            moveCosts[(2*dimension)+1] = sigmoid(sum);
+
+        }
+
+        //  Next determine all possible directions (cheapest) we can move in
+        double minValue = Double.MAX_VALUE;
+        double backValue;
+        double frontValue;
+        byte[] viableDirections = new byte[numDimensions];
+        for(int dimension = 0; dimension<numDimensions; dimension++) {
+
+            backValue = moveCosts[2*dimension];
+            frontValue = moveCosts[(2*dimension) + 1];
+
+            if(backValue < minValue || frontValue < minValue) {
+                Arrays.fill(viableDirections, (byte)0);
+
+                if(backValue < frontValue) {
+                    viableDirections[dimension] = BACKWARD;
+                    minValue = backValue;
+                } else {
+                    viableDirections[dimension] = FORWARD;
+                    minValue = frontValue;
+                }
+            } else if (backValue == minValue || frontValue == minValue) {
+                if(backValue == minValue && frontValue == minValue) {
+                    viableDirections[dimension] = BACKWARD_FORWARD;
+                } else {
+                    if(backValue == minValue) {
+                        viableDirections[dimension] = BACKWARD;
+                    } else {
+                        viableDirections[dimension] = FORWARD;
+                    }
+                }
+            }
+
+        }
+
+        //  Step 3a:  Determine all the directions we can move in
+        int directionCount = 0;
+        int lastViableDirectionIndex = -1;
+        for(int d = 0; d<numDimensions; d++) {
+            if(viableDirections[d] > 0) {
+                directionCount ++;
+                lastViableDirectionIndex = d;
+            }
+        }
+        int[] directionLocations = new int[directionCount];
+        int dirIndex = 0;
+        for(int d = 0; d<numDimensions; d++) {
+            if(viableDirections[d] > 0) {
+                directionLocations[dirIndex] = d;
+                dirIndex ++;
+            }
+        }
+
+        Random random = new Random(System.nanoTime());
+        int dimensionToMoveAlong = 0;
+        boolean moveForward = false;
+        int nextDirectionIndex = directionCount > 1 ? random.nextInt(directionCount) : lastViableDirectionIndex;
+        dimensionToMoveAlong = nextDirectionIndex;
+        if(viableDirections[nextDirectionIndex] == BACKWARD_FORWARD) {
+            moveForward = random.nextBoolean();
+        } else {
+            moveForward = viableDirections[nextDirectionIndex] == FORWARD;
+        }
+
+        //  Step 4:  Make the move
+        if(moveForward){
+            currentLocationInSpace[dimensionToMoveAlong] += 1;
+        } else {
+            currentLocationInSpace[dimensionToMoveAlong] -= 1;
+        }
+
+        //  Step 5:  Make sure we're not out of bounds
+        if(currentLocationInSpace[dimensionToMoveAlong] < 0) {
+            currentLocationInSpace[dimensionToMoveAlong] = numPoints-1;
+        } else {
+            currentLocationInSpace[dimensionToMoveAlong] %= numPoints;
+        }
+
+
+        return currentLocationInSpace;
+    }
+}
---
+++ src/test/java/com/vandenbreemen/cogthing/application/LocalMinimaWithoutGridTest.kt
@@ -0,0 +1,87 @@
+package com.vandenbreemen.cogthing.application
+
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+internal class LocalMinimaWithoutGridTest {
+
+    @Test
+    fun `should calculate next move to lowest direction - right`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(2, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5)
+        seeker.setAdjacentValues(1.0, 0.0, 2.0, 2.0)    //  1 <-- --> 0 along X axis, 2 <-- --> 2 along Y axis
+
+        val next = seeker.nextLocation
+        next.asList() shouldBeEqualTo listOf(6, 5)      //  since Lowest cost positive X direction
+    }
+
+    @Test
+    fun `should calculate next move to lowest direction - left`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(2, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5)
+        seeker.setAdjacentValues(0.5, 1.0, 2.0, 2.0)
+
+        val next = seeker.nextLocation
+        next.asList() shouldBeEqualTo listOf(4, 5)      //  since Lowest cost positive X direction
+    }
+
+    @Test
+    fun `should calculate next move to lowest direction - up`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(2, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5)
+        seeker.setAdjacentValues(2.0, 2.0, 1.0, 0.5)
+
+        val next = seeker.nextLocation
+        next.asList() shouldBeEqualTo listOf(5, 6)
+    }
+
+    @Test
+    fun `should calculate next move to lowest direction - down`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(2, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5)
+        seeker.setAdjacentValues(2.0, 2.0, 0.5, 1.5)
+
+        val next = seeker.nextLocation
+        next.asList() shouldBeEqualTo listOf(5, 4)
+    }
+
+    @Test
+    fun `should work in more dimensions`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(4, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5, 6, 6)
+        seeker.setAdjacentValues(2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 1.0)
+
+        val next = seeker.nextLocation
+        next.asList() shouldBeEqualTo listOf(5, 5, 6, 7)
+    }
+
+    @Test
+    fun `should try random direction if all directions have same cost`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(2, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5)
+        seeker.setAdjacentValues(0.0, 0.0, 0.0, 0.0)
+
+        for(i in 1 until 10) {
+            println(seeker.nextLocation.asList())
+        }
+    }
+
+    @Test
+    fun `should try random direction if two or more directions have same minimal cost`() {
+        val seeker = LocalMinimaSeekerWithoutGrid(2, 100)
+
+        seeker.setCurrentLocationInSpace(5, 5)
+
+        for(i in 1 until 10) {
+            seeker.setAdjacentValues(1.0, 0.0, 0.0, 0.0)
+            println(seeker.nextLocation.asList())
+        }
+    }
+
+}
